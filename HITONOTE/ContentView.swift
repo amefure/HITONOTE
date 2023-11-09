bec37829c058f65cc1d19454a530ab2ddf6b9027
amefure <shachi10969@gmail.com>
2023-11-09 09:05:01 +0900

    Initial Commit

---
+++ HITONOTE/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HITONOTE
+//
+//  Created by t&a on 2023/11/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HITONOTE/HITONOTEApp.swift
@@ -0,0 +1,17 @@
+//
+//  HITONOTEApp.swift
+//  HITONOTE
+//
+//  Created by t&a on 2023/11/09.
+//
+
+import SwiftUI
+
+@main
+struct HITONOTEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
